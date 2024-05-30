77d5c4e98e51159f3d7c20cd7481898ec54de151
RIYA ORANG <101129794+riya-orang@users.noreply.github.com>
2024-05-31 02:19:38 +0530

    Update user.js

--- client/src/pages/user.js
+++ client/src/pages/user.js
@@ -34,7 +34,7 @@ export default function User() {
     };
     console.log(user);
     axios
-      .post("http://localhost:5000/users/add", user)
+      .post("https://motion-canvas-three.vercel.app/users/add", user)
       .then((res) => console.log(res.data));
 
     setUsername("");
